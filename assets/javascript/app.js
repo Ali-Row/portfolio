d6b8064022388d3ce3f1a4a3344cc6653157b347
Alistair <deadlyhardware@gmail.com>
2020-10-18 21:52:22 -0400

    Huge updates, intro animation with blur, card hovers implemented, some design tweaks

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,36 @@
 AOS.init();
+
+$(document).ready(() => {
+    $('.body-transition').hide();
+    
+    let renderAnimation = () => {
+        intro = $('<h1>');
+        intro.addClass('space-mono accent-color px-18 skew rounded animated zoomInUp slow intro-element');
+        intro.text('A');
+        let container = $('<div>');
+        container.addClass('lead-container');
+        let row = $('<div>');
+        row.addClass('row text-center d-flex justify-content-center');
+        let col = $('<div>');
+
+        col.addClass('col-md-12 d-flex justify-content-center');
+        col.append(intro);
+        row.append(col);
+        container.append(row);
+        
+        $('body').append(container);
+
+    }
+    renderAnimation();
+
+    let addZoomOut = () => {
+        intro.addClass('animated zoomOut blur');
+    }
+
+    let showPortfolio = () => {
+        $('.intro-element').hide();
+        $('.body-transition').show();
+    }
+    addZoomOutClass = setTimeout(addZoomOut, 3000);
+    show = setTimeout(showPortfolio, 4300);
+})
