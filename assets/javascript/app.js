d8c10518717219b7bfbb69fc76b37a31f8f1d8c8
Alistair <deadlyhardware@gmail.com>
2020-10-19 20:53:04 -0400

    Clean up Js and add comments, use the min file for the CSS and clean up CSS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,36 +1,34 @@
-AOS.init();
-
 $(document).ready(() => {
-    $('.body-transition').hide();
-    
-    let renderAnimation = () => {
-        intro = $('<h1>');
-        intro.addClass('space-mono accent-color px-18 skew rounded animated zoomInUp slow intro-element');
-        intro.text('A');
-        let container = $('<div>');
-        container.addClass('lead-container');
-        let row = $('<div>');
-        row.addClass('row text-center d-flex justify-content-center');
-        let col = $('<div>');
+    AOS.init();
+  $(".body-transition").hide();
 
-        col.addClass('col-md-12 d-flex justify-content-center');
-        col.append(intro);
-        row.append(col);
-        container.append(row);
-        
-        $('body').append(container);
+  let renderAnimation = () => {
+    intro = $("<h1>");
+    intro.addClass("space-mono accent-color px-18 skew rounded animated zoomInUp slow intro-element");
+    intro.text("A");
+    let container = $("<div>");
+    container.addClass("lead-container");
+    let row = $("<div>");
+    row.addClass("row text-center d-flex justify-content-center");
+    let col = $("<div>");
+    col.addClass("col-md-12 d-flex justify-content-center");
+    col.append(intro);
+    row.append(col);
+    container.append(row);
+    $("body").append(container);
+  };
+  renderAnimation();
 
-    }
-    renderAnimation();
+  let addZoomOut = () => {
+    intro.addClass("animated zoomOut blur");
+  };
 
-    let addZoomOut = () => {
-        intro.addClass('animated zoomOut blur');
-    }
-
-    let showPortfolio = () => {
-        $('.intro-element').hide();
-        $('.body-transition').show();
-    }
-    addZoomOutClass = setTimeout(addZoomOut, 3000);
-    show = setTimeout(showPortfolio, 4300);
-})
+  // Renders the portfolio, this runs after the intro animation is over
+  let renderPortfolio = () => {
+    $(".intro-element").hide();
+    $(".body-transition").show();
+  };
+  // These setTimeouts call the addZoomOut function first then finally the renderPortfolio function
+  addZoomOutClass = setTimeout(addZoomOut, 3000);
+  show = setTimeout(renderPortfolio, 4300);
+});
